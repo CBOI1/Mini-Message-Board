cbdd56b46790428c0dbc9210fa14dfd3a7a1cc6b
Christian Perez <christianperez9720@gmail.com>
2025-11-25 17:55:05 -0800

    Change port to 8000
    Deployment

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 const indexRouter = require("./routes/indexRouter");
 app.use("/", indexRouter);
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT);
